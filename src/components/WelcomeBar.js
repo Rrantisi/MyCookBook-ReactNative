9471fdceef9d086a9185c83d35cbd57480a7af16
Rrantisi <rantisireham19@gmail.com>
2023-12-31 17:46:24 -0500

    creates welcomebar placeholder

---
+++ src/components/WelcomeBar.js
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function WelcomeBar() {
+  return (
+    <View>
+      <Text>WelcomeBar</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
