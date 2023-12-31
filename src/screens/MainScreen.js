d0fd4b6ec103214de1965e770db965561401ede1
Rrantisi <rantisireham19@gmail.com>
2023-12-31 17:45:23 -0500

    defines components obj and adds jsx to handle rendering components and adds styling

--- src/screens/MainScreen.js
+++ src/screens/MainScreen.js
@@ -1,12 +1,71 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, View, SafeAreaView, ActivityIndicator, ImageBackground } from 'react-native';
+import React, {useState} from 'react';
+import bgImage from '../../assets/images/bg1.png';
+import HomeScreen from '../screens/HomeScreen';
+import SavedScreen from '../screens/SavedScreen';
+import FavoritesScreen from '../screens/FavoritesScreen';
+import ProfileScreen from '../screens/ProfileScreen';
+import WelcomeBar from '../components/WelcomeBar';
 
-export default function MainScreen() {
+export default function MainScreen({ activeTab }) {
+  const [isImageLoaded, setImageLoaded] = useState(false);
+
+  // defines the components object to be used for rendering activeTab component:
+  const components = {
+    home: <HomeScreen />,
+    saved: <SavedScreen />,
+    favorites: <FavoritesScreen />,
+    profile: <ProfileScreen />
+  }
+
+  // handles bg image loading
+  const handleImageLoad = () => {
+    // Image has loaded, update the state
+    setImageLoaded(true);
+  };
+
   return (
-    <View>
-      <Text>MainScreen</Text>
+<View style={styles.container}>
+  <ImageBackground
+    source={bgImage} 
+    style={styles.backgroundImage}
+    resizeMode="cover" 
+    onLoad={handleImageLoad} >
+
+  {!isImageLoaded && (
+    <SafeAreaView style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: 'rgba(0, 0, 0, 0.85)' }}>
+      <ActivityIndicator size="large" />
+    </SafeAreaView>
+  )}
+
+  {isImageLoaded && (
+    <View style={styles.textContainer}>
+       <WelcomeBar />
+        {components[activeTab]}
     </View>
+  )}
+  </ImageBackground>
+
+  </View>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  backgroundImage: {
+    width: '100%',
+    height: '100%',
+  },
+  textContainer: {
+    flex: 1,
+    backgroundColor: 'rgba(0, 0, 0, 0.45)'
+  },
+  text: {
+    color: 'white',
+    fontSize: 25,
+  }
+});
