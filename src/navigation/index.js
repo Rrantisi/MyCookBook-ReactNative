c0b0b0bacaa1808e37283b3417571a390d7ba469
Rrantisi <rantisireham19@gmail.com>
2023-12-31 19:00:47 -0500

    adds search and toolscreen screens to navigator stack

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -3,6 +3,8 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import WelcomeScreen from '../screens/WelcomeScreen';
 import BottomTabs from './BottomTabs';
+import SearchScreen from '../screens/SearchScreen';
+import ToolScreen from '../screens/ToolScreen';
 
 const Stack = createNativeStackNavigator();
 
@@ -12,6 +14,8 @@ export default function AppNavigation() {
       <Stack.Navigator initialRouteName="Welcome" screenOptions={{headerShown: false, gestureEnabled: false}} >
         <Stack.Screen name="Welcome" component={WelcomeScreen} />
         <Stack.Screen name="Main" component={BottomTabs} />
+        <Stack.Screen name="Search" component={SearchScreen} />
+        <Stack.Screen name="ToolScreen" component={ToolScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   )
